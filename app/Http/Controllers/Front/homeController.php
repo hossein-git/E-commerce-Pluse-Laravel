a652b006451b7c7cccf85ea4758059b311328a7e
hossein-git <hosseinhaghparast0@gmail.com>
2020-01-01 23:10:57 +0300

    show rating problem fixed

--- app/Http/Controllers/Front/homeController.php
+++ app/Http/Controllers/Front/homeController.php
@@ -93,7 +93,7 @@ class homeController extends Controller
         $this->inputs($request);
         $products = $this->product->orderBy("$request->sort", "$request->dcs")
             ->whereBetween('sale_price', [$request->priceMin, $request->priceMax])
-            ->select(['product_slug', 'product_name', 'description', 'status',
+            ->select(['product_id','product_slug', 'product_name', 'description', 'status',
                     'data_available', 'is_off', 'off_price', 'cover', 'sale_price', 'created_at']
             )->paginate($request->paginate);
 
